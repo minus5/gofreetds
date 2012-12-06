cb82545d8d423c778088bbcf38e2c6d523f27cc0
ianic <igor.anic@gmail.com>
2012-12-06 18:47:57 +0100

    testing stored procedure return value

--- freetds_test.go
+++ freetds_test.go
@@ -2,7 +2,7 @@ package freetds
 
 import ("testing"; "os";"fmt")
 
-const CREATE_DB_SCRIPT = `
+var CREATE_DB_SCRIPTS = [...]string{`
 if exists(select * from sys.tables where name = 'freetds_types')
 drop table freetds_types
 ;
@@ -36,9 +36,12 @@ insert into freetds_types (int, long, smallint, tinyint, varchar, nvarchar, char
 values (-2147483648, -9223372036854775808, -32768,  0, 'nije reko dobar dan','nije reko dobar dan 2','nije reko dobar dan 3','nije reko dobar dan 4','nije reko dobar dan 5','nije reko dobar dan 6', '1998-10-10T16:17:18','1998-10-10T16:17:18', -1234.5678, -1234.5678, -1234.5678, -1234.5678, 1, 0x0987654321abcd)
 
 insert into freetds_types (int) values (3)
-;
-
-`
+`,`
+if exists(select * from sys.procedures where name = 'freetds_return_value')
+  drop procedure freetds_return_value
+`,`
+create procedure freetds_return_value as
+  return -5`}
 
 func ConnectToTestDb(t *testing.T) (*Conn) {
   db   := os.Getenv("GOFREETDS_DB")
@@ -66,9 +69,26 @@ func TestCreateTable(t *testing.T) {
     return
   }
   defer conn.Close()
-  _, err := conn.Exec(CREATE_DB_SCRIPT)
+  for _, s := range CREATE_DB_SCRIPTS {
+    _, err := conn.Exec(s)
+    if err != nil {
+      t.Error(err)
+    }
+  }
+}
+
+func TestStoredProcedureReturnValue(t *testing.T) {
+  conn := ConnectToTestDb(t)
+  if conn == nil {
+    return
+  }
+  defer conn.Close()
+  results, err := conn.Exec("exec freetds_return_value")
   if err != nil {
     t.Error(err)
+  }
+  if results[0].ReturnValue != -5 {
+    t.Errorf("expected return value was -5 got %d", results[0].ReturnValue)
   }
 }
 
