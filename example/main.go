9a96c4c1bf551fdb139e7ed92c06ba28d284950a
ianic <igor.anic@gmail.com>
2012-12-19 15:04:40 +0100

    Updated example

--- example/main.go
+++ example/main.go
@@ -3,6 +3,7 @@ package main
 import ("gofreetds";"fmt")
 
 func main() {
+  //conect
   conn, err := freetds.Connect("ianic", "ianic", "iow", "pubs")
   if err != nil {
     fmt.Printf("error: %s\n%s\n%s", err, conn.Error, conn.Message)
@@ -10,20 +11,22 @@ func main() {
   }
   defer conn.Close()
 
+  //execute query
   rst, err := conn.Exec("select au_id, au_lname, au_fname from authors")
   if err != nil {
     fmt.Printf("error %s", err)
     return
   }
+  //print query result, columns
   for _,c := range rst[0].Columns {
     fmt.Printf("%-20s", c.Name)
   }
   fmt.Printf("\n")
+  //rows
   for _,r := range rst[0].Rows {
     for _,v := range r {
       fmt.Printf("%-20v", v)
     }
     fmt.Printf("\n")
   }
-
 }
